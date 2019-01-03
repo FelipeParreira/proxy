11b8369731174d36274dda07631a4e4f20918b1a
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-03 13:58:45 -0800

    Use fecth

--- routes/test.js
+++ routes/test.js
@@ -5,11 +5,15 @@ const router = express.Router();
 /* GET test */
 router.get('/', (req, res, next) => {
   // res.send('Response from API GET request!ES6');
-  $.ajax({
-    type: 'GET',
-    url: 'http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/testQA',
-    success: data => res.send(data),
-    error: (err) => console.log('error!!!!', err)
+  // $.ajax({
+  //   type: 'GET',
+  //   url: 'http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/testQA',
+  //   success: data => res.send(data),
+  //   error: (err) => console.log('error!!!!', err)
+  // });
+  fetch('http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/testQA')
+  .then(data => {
+    res.send(data);
   });
 });
 
