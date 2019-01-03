83dfad9b3a811e55526dbdeb2f6f3ba96fc9576a
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-03 13:29:51 -0800

    Connect to the QA server

--- routes/test.js
+++ routes/test.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const $ = require('jquery');
+const router = express.Router();
 
 /* GET test */
 router.get('/', (req, res, next) => {
-  res.send('Response from API GET request!ES6');
+  // res.send('Response from API GET request!ES6');
+  $.ajax({
+    type: 'GET',
+    url: 'http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/testQA',
+    success: data => res.send(data),
+    error: (err) => console.log('error', err)
+  });
 });
 
 module.exports = router;
