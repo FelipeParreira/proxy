df3ba6b2c8b5b8629efe1313142452bae10c5813
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-02 22:25:58 -0800

    Handle test route with router

--- routes/test.js
+++ routes/test.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET test */
-router.get('/test', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.send('Response from API GET request!');
 });
 
