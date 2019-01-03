f564d54becc35c12207d1b52f7788550dcbb24cf
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-03 14:26:55 -0800

    Change node command

--- routes/test.js
+++ routes/test.js
@@ -1,18 +1,18 @@
 const express = require('express');
 // const request = require('request-promise');
 // const fetch = require('node-fetch');
-// const $ = require('jquery');
+const $ = require('jquery');
 const router = express.Router();
 
 /* GET test */
 router.get('/', (req, res, next) => {
-  res.send('Response from API GET request!ES6');
-  // $.ajax({
-  //   type: 'GET',
-  //   url: 'http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/testQA',
-  //   success: data => res.send(data),
-  //   error: (err) => console.log('error!!!!', err)
-  // });
+  // res.send('Response from API GET request!ES6');
+  $.ajax({
+    type: 'GET',
+    url: 'http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/testQA',
+    success: data => res.send(data),
+    error: (err) => console.log('error!!!!', err)
+  });
   // const response = await fetch('http://localhost:3000/hotels/4/questions');
   //   questions = await response.json();
   // fetch('http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/testQA')
