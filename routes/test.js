a6d8e496abb5f93351201e711593c7133379301b
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-03 14:13:46 -0800

    go back to res.send

--- routes/test.js
+++ routes/test.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 /* GET test */
 router.get('/', (req, res, next) => {
-  // res.send('Response from API GET request!ES6');
+  res.send('Response from API GET request!ES6');
   // $.ajax({
   //   type: 'GET',
   //   url: 'http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/testQA',
@@ -19,10 +19,10 @@ router.get('/', (req, res, next) => {
   // .then(data => {
   //   res.send(data);
   // });
-  request.get('http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/testQA')
-      .on('response', (data) => {
-        res.send(data);
-      });
+  // request.get('http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/testQA')
+  //     .on('response', (data) => {
+  //       res.send(data);
+  //     });
 });
 
 module.exports = router;
