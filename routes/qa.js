8f22ec5ca3ff22c72538ce27ada6a94c71a27102
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-03 22:08:34 -0800

    Remove stub response

--- routes/qa.js
+++ routes/qa.js
@@ -18,8 +18,7 @@ router.get('/', (req, res, next) => {
 router.get('/hotels/:hotelId/questions', (req, res, next) => {
   // console.log('inside get request of qa route for questions [proxy server]');
   const { hotelId } = req.params;
-  res.send([]);
-  // res.redirect(`http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/hotels/${hotelId}/questions`);
+  res.redirect(`http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/hotels/${hotelId}/questions`);
 });
 
 module.exports = router;
