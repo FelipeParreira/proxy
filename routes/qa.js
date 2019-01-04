853aec930a0d6db88151bc2bc675d48506a6d8bc
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-03 21:44:50 -0800

    Add new header to response

--- routes/qa.js
+++ routes/qa.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,POST');
   next();
 });
 
