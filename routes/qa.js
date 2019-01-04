e0ab82a7340a1e4c96078ee8078009691b2ae4e1
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-03 22:03:29 -0800

    Stub sending response back

--- routes/qa.js
+++ routes/qa.js
@@ -18,7 +18,8 @@ router.get('/', (req, res, next) => {
 router.get('/hotels/:hotelId/questions', (req, res, next) => {
   // console.log('inside get request of qa route for questions [proxy server]');
   const { hotelId } = req.params;
-  res.redirect(`http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/hotels/${hotelId}/questions`);
+  res.send([]);
+  // res.redirect(`http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/hotels/${hotelId}/questions`);
 });
 
 module.exports = router;
