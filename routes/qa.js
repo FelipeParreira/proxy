0a2180dbe68486a5cb83c7710b1531af1d66b619
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-05 11:11:29 -0800

    Send response without response.data

--- routes/qa.js
+++ routes/qa.js
@@ -51,7 +51,7 @@ router.delete('/hotels/:hotelId/questions/:questionId', (req, res) => {
     timeout: 60000,
   })
   .then(response => {
-    res.send(reponse.data);
+    res.send(reponse);
   })
   .catch(error => {
     res.send(error);
