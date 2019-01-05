dbcf4178488583f91726942d6bc1ca459a0a4cd3
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-04 19:07:34 -0800

    Add stub response

--- routes/qa.js
+++ routes/qa.js
@@ -19,10 +19,11 @@ router.get('/', (req, res, next) => {
 router.get('/hotels/:hotelId/questions', (req, res, next) => {
   // console.log('inside get request of qa route for questions [proxy server]');
   const { hotelId } = req.params;
-  axios.get(`http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/hotels/${hotelId}/questions`)
-  .then(response => {
-    res.send(response);
-  });
+  res.send(['aba']);
+  // axios.get(`http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/hotels/${hotelId}/questions`)
+  // .then(data => {
+  //   res.send(data);
+  // });
   // res.redirect(`http://node-express-env-service-qa.swpb5j5env.us-west-2.elasticbeanstalk.com/hotels/${hotelId}/questions`);
 });
 
